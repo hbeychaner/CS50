ad85a21d4f068341f975371a6ca1d834f616ebf4
hbeychaner <hbeychaner@users.noreply.github.com>
2018-09-29 17:11:25 +0000

    20180929T171110Z

--- application.py
+++ application.py
@@ -189,8 +189,8 @@ def logout():
 def quote():
     """Get stock quote."""
     if request.method == "POST":
-        if not lookup(request.form.get("symbol")):
-            return apology("Symbol does not exist", 403)
+        if not lookup(request.form.get("symbol")) or not request.form.get("symbol"):
+            return apology("Symbol does not exist", 400)
         quote = lookup(request.form.get("symbol"))
         quote["price"] = usd(quote["price"])
         return render_template("quoted.html", chart=quote)
@@ -207,14 +207,14 @@ def register():
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username", 403)
+            return apology("must provide username", 400)
 
         # Ensure password was submitted
         if not request.form.get("password"):
-            return apology("must provide password", 403)
+            return apology("must provide password", 400)
 
         if request.form.get("password") != request.form.get("confirmation"):
-            return apology("Password and confirmation do not match.", 403)
+            return apology("Password and confirmation do not match.", 400)
 
         if not db.execute("SELECT * FROM users WHERE username = :username", username=request.form.get("username")):
             db.execute("INSERT INTO users (username, hash) VALUES (:u, :h)", u=request.form.get("username"), h=generate_password_hash(request.form.get("password")))
@@ -222,7 +222,7 @@ def register():
             session["user_id"] = rows[0]["id"]
             return render_template("/register.html")
         else:
-            return apology("Username already exists.", 405)
+            return apology("Username already exists.", 400)
     else:
         return render_template("register.html")
 
