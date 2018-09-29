ec8b06678d41ffdbd28fa94880e7c6873e6fd05d
hbeychaner <hbeychaner@users.noreply.github.com>
2018-09-29 16:54:54 +0000

    20180929T165421Z

--- helpers.c
+++ helpers.c
@@ -1,107 +1,96 @@
-// Helper functions for music
 #include <string.h>
 #include <cs50.h>
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
-#include <cs50.h>
-#include <math.h>
-#include "helpers.h"
 
-// Converts a fraction formatted as X/Y to eighths
-int duration(string fraction)
+// This function measures whether a note on a line is a rest or not. If it's a rest, return 1. Otherwise, return 0.
+_Bool is_rest(char * line)
 {
-    const int BASE = (8);
-    int numerator = atoi(&fraction[0]);
-    int denominator = atoi(&fraction[2]);
-
-	return (numerator * BASE) / denominator;
-}
-
-// Calculates frequency (in Hz) of a note
-int frequency(string note)
-{
-    int accidental;
-    int octave;
-    int steps;
-    const int MID_A = 440;
-
-    switch(note[0])
-        {
-            case 'C'  :
-            steps = -9;
-            break;
-
-            case 'D'  :
-            steps = -7;
-            break;
-
-            case 'E'  :
-            steps = -5;
-            break;
-
-            case 'F'  :
-            steps = -4;
-            break;
-
-            case 'G'  :
-            steps = -2;
-            break;
-
-            case 'A'  :
-            steps = 0;
-            break;
-
-            case 'B'  :
-            steps = 2;
-            break;
-        }
-
-    if (note[1] == '#')
-    {
-        accidental = 1;
-        octave = atoi(&note[2]);
-    }
-    else if (note[1] == 'b')
-    {
-        accidental = -1;
-        octave = atoi(&note[2]);
-    }
-    else
-    {
-        accidental = 0;
-        octave = atoi(&note[1]);
-    }
-
-    // Defines distance in semitones between A4 and the input.
-    int diff = (accidental + ((octave - 4) * 12) + steps);
-
-    // If 0 difference, return A4 frequency. Else, this calculates the frequency and returns it, rounded to the nearest int.
-    int frequency = 0;
-    float power = (float)diff / 12;
-    if (octave >= 0)
-    {
-        frequency = round(MID_A * pow(2, power));
-    }
-    else
-    {
-        frequency = round(MID_A / pow(2, power));
-    }
-
-    return frequency;
-}
-
-// Determines whether a string represents a rest
-bool is_rest(string s)
-{
-    // Tests whether string has an alpha character. If not, it's empty and therefore a rest.
-    if (!isalpha(s[0]))
+	if (!isalpha(line[0]))
     {
 		return 1;
 	}
-	// If first character is a letter, return 0 to denote a note.
     else
 	{
         return 0;
 	}
+}
+
+// This function returns an integer equal to the duration of a note in 16ths.
+int duration(char * line)
+{
+        const float BASE = (1/8);
+        int numerator = atoi(&line[0]);
+        int denominator = atoi(&line[2]);
+
+		return numerator / denominator / BASE;
+}
+
+// This function reads a note from a string and translates it into a frequency to be passed to the main function.
+int frequency(char * line)
+{
+    int note;
+    int accidental;
+    int octave;
+    const int MID_A = 440;
+
+        switch(line[0])
+        {
+            case 'C'  :
+            note = -9;
+            break;
+
+            case 'D'  :
+            note = -7;
+            break;
+
+            case 'E'  :
+            note = -5;
+            break;
+
+            case 'F'  :
+            note = -4;
+            break;
+
+            case 'G'  :
+            note = -2;
+            break;
+
+            case 'A'  :
+            note = 0;
+            break;
+
+            case 'B'  :
+            note = 2;
+            break;
+        }
+
+    if (line[1] == '#')
+    {
+        accidental = 1;
+        octave = line[2];
+    }
+    else if (line[1] == 'b')
+    {
+        accidental = -1;
+        octave = line[2];
+    }
+    else
+    {
+        accidental = 0;
+        octave = line[1];
+    }
+
+    int diff = (accidental + ((octave - 3) * 12) + note);
+
+    if (diff > 0)
+    {
+        return MID_A * (2 ^ (diff / 12));
+    }
+    else if (diff < 0)
+    {
+        return MID_A / (2 ^ (diff / 12));
+    }
+    else return MID_A;
 }
